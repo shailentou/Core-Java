1f70e1ad27746cf1649756014f3698e9ad19cc18
Admin <Admin@MXITL029.MAXIMAPUNE.COM>
2021-01-19 22:42:08 +0530

    commit

--- Core-Java/src/com/shail/MultiThreading/WorkerThread.java
+++ Core-Java/src/com/shail/MultiThreading/WorkerThread.java
@@ -1,10 +1,38 @@
 package com.shail.MultiThreading;
 
-public class WorkerThread {
+public class WorkerThread implements Runnable {
 
+	
+	private String command;
+	
+	public WorkerThread(String command) {
+	
+		this.command=command;
+	}
+	
+	private void processCommand() {
+		try {
+			Thread.sleep(5000);
+		}
+		catch(InterruptedException e) {
+			e.printStackTrace();
+		}
+		
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+	}
+
+	@Override
+	public void run() {
+		System.out.println(Thread.currentThread().getName() + " Start Command = " + command);
+		processCommand();
+		System.out.println(Thread.currentThread().getName() + " End Command = " + command);
+	}
+	
+	public String toString() {
+		return command;
 	}
 
 }
